606e79ed439405474c80de7ff1b6a9bc13e4f517
lionawake <fxh1121@163.com>
2018-05-11 14:14:53 +0800

    another function&parameters list

--- double_avg.py
+++ double_avg.py
@@ -4,7 +4,12 @@ from HiCommon import *
 
 print('Start')
 arg_list = []
-
+'''
+args: cash, block, [query:para_list], [tactics_id, tactics_desc], func_list
+func_list: [func_num, func1, para_num1, para_list1, func2, para_num2, para_list2, ...]
+para_list: ['min1, max1, step1', 'min2, max2, step2', ...], len(para_list) == para_num
+'''
+#arg_list.append(HikyuuArgs(80000000, blocka, ['-50,-100,-5'], ['001','tactics001'], [2, 'SG_Flex', 2, ['1,5,2', '10,20,3'], 'MM_FixedCount', 1, ['1000,1000,1']]))
 arg_list.append(HikyuuArgs(80000000, blocka, ['-50,-100,-5'], 'SG_Flex', ['1,5,2', '10,20,3'], 'MM_FixedCount', ['1000,1000,1'], ['001','tactics001']))
 #arg_list.append(HikyuuArgs(90000000, 'SG_Flex', [5, 10], 'MM_FixedCount', [2000], blockg, -60, '002', 'tactics002'))
 #arg_list.append(HikyuuArgs(20000000, 'SG_Flex', [1, 20], 'MM_FixedCount', [3000], blocksh, -90, '003', 'tactics003'))
