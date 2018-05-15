b36de2d0c3c3bdd401cd22bf2a3d0b0211bca42a
lionawake <fxh1121@163.com>
2018-05-15 09:50:04 +0800

    modify filter class&policy generate

---
+++ LqFilter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#==============================================================================
+# written by fuxuanhuang
+#==============================================================================
+from hikyuu.interactive.interactive import *
+
+class StockFilter:
+    '筛选公共类'
+    def __init__(self, stock):
+        #print(self.__doc__)
+        pass
+    def __del__(self):
+        pass
+
+    def stock_filter001(self):
+        return 1000
+
+    def stock_filter002(self):
+        return 100
+
+    def stock_filter003(self):
+        pass
+
---
+++ LqPolicyGen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#==============================================================================
+# written by fuxuanhuang
+#==============================================================================
+import os
+import time
+import datetime
+
+policy_id = int(time.time() * 1000000)
+policy_desc = "tactics_%d" % policy_id
+py_file = "%s.py" % policy_desc
+print(py_file)
+py_fd = open(py_file, "w+")
+
+py_str = "from hikyuu.interactive.interactive import *\n" \
+         "import LqFilter as lqf\n\n" \
+         "stocks=[]\n" \
+         "for s in blocka:\n" \
+         "    if lqf.StockFilter(s).stock_filter001() >= 100 and lqf.StockFilter(s).stock_filter002() < 1000:\n" \
+         "        stocks.append(s)\n" \
+         "        #print(s)\n" \
+         "print(stocks)\n"
+
+py_fd.write(py_str)
+py_fd.close()
+
+#执行通过替换指标函数后的py代码文件
+local_dir = os.getcwd()
+py_exe = local_dir + "\\venv\Scripts\python.exe"
+os.system("%s " % py_exe + "%s" % py_file)
