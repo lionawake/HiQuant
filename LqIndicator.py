a0a763bcd6c36c822613c1fa3330731d47dd5679
lionawake <fxh1121@163.com>
2018-07-13 18:09:42 +0800

    删除0号函数

--- LqIndicator.py
+++ LqIndicator.py
@@ -7,10 +7,6 @@
 
 import talib
 
-def func_return_input(p):
-	'''00000,1,1'''
-	return p
-
 def lqAMA(data, n=10, fast_n=2, slow_n=30):
     '''00001,4,2'''
     return AMA(data, n=10, fast_n=2, slow_n=30)
